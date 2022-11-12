8410b11284db3d87de29338130cec0883198adeb
IniguezMarc <miniguezbarbero@gmail.com>
2022-11-12 22:45:36 +0100

    typing animation && dark/light theme

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    console.log(entry);
     if (entry.isIntersecting) {
-      entry.target.classList.add("show");
+      console.log(entry.target)
+      if ( entry.target.hasAttribute('typed')) {
+        entry.target.classList.add("typing-animation");
+        let length = entry.target.textContent.length;
+        entry.target.style.animationTimingFunction = 'steps('+length+',end)';
+        entry.target.style.animationDuration = '1s';
+      }else{
+        entry.target.classList.add("show");
+      }
     } else {
-      entry.target.classList.remove("show");
+      if ( entry.target.hasAttribute('typed')) {
+        entry.target.classList.remove("typing-animation");
+      }else{
+        entry.target.classList.remove("show");
+      }
     }
   });
 });
@@ -38,24 +49,24 @@ function rain() {
       rainElement.alt = "";
       rainElement.style.top = Math.random() * 100 + "%";
       rainElement.style.animation =
-        "rainAnimation " + randomIntFromInterval(13, 35) + "s 2";
+        "rainAnimation " + randomIntFromInterval(20, 40) + "s 2";
       rainElement.style.animationIterationCount = "infinite";
+      rainElement.style.animationDelay = "-100s";
       body.appendChild(rainElement);
     }
   });
 }
 
-const toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
+  document.querySelector('.themeSwitch').addEventListener("click", function(e) {
+  let theme = document.documentElement.getAttribute('data-theme');
+  if (theme == "dark") {
+      document.documentElement.setAttribute('data-theme', 'light');
+      e.target.src = "assets/icons/bulbOff.png";
+  }
+  else {
+      document.documentElement.setAttribute('data-theme', 'dark');
+      e.target.src = "assets/icons/bulbOn.png";
+  }    
+});
 
-function switchTheme(e) {
-    if (e.target.checked) {
-      console.log('theme dark');
-        document.documentElement.setAttribute('data-theme', 'dark');
-    }
-    else {
-      console.log('theme light');
-        document.documentElement.setAttribute('data-theme', 'light');
-    }    
-}
 
-toggleSwitch.addEventListener('change', switchTheme, false);
